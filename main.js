8e792115d0989a4946e4ff8c926047c67050a214
Karim Shaloh <kshaloh@gmail.com>
2024-03-24 01:13:11 -0400

    working progress

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', addEventListeners);
 
 function addEventListeners() {
 	const dateP = document.getElementById('current-date');
-	const timeElement = document.getElementById('current-time');
+	const hoursP = document.getElementById('current-hours');
+	const minutesP = document.getElementById('current-minutes');
 
 	const date = new Date(Date.now());
 
@@ -14,10 +15,13 @@ function addEventListeners() {
 		const minutes = now.getMinutes();
 		const seconds = now.getSeconds();
 		// Format the string with leading zeroes
-		const clockStr = `${hours.toString().padStart(2, '0')}:${minutes
+		const hoursStr = `${hours.toString().padStart(2, '0')}`;
+		const minuitesStr = `${minutes.toString().padStart(2, '0')}:${seconds
 			.toString()
-			.padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-		timeElement.innerText = clockStr;
+			.padStart(2, '0')}`;
+
+		hoursP.innerText = hoursStr;
+		minutesP.innerText = minuitesStr;
 	}
 	updateTime();
 	setInterval(updateTime, 1000);
